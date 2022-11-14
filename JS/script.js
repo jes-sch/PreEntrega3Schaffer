4a1cd8d9f6b794da93ceed7abe991dafcabff3ed
Jes Sch <jesica_schaffer@hotmail.com>
2022-11-14 00:16:58 -0300

    creo primer repo

---
+++ JS/script.js
@@ -0,0 +1,94 @@
+const btnCalcular = document.getElementById("btnCalcular"),
+resultadosFinales1Element = document.getElementById("ResultadosFinales1"),
+resultadosFinales2Element = document.getElementById("ResultadosFinales2"),
+resultadosFinales3Element = document.getElementById("ResultadosFinales3"),
+resultadosFinales4Element = document.getElementById("ResultadosFinales4"),
+resultadosFinales5Element = document.getElementById("ResultadosFinales5"),
+btnRecuperarUltimo = document.getElementById("btnCalcular"),
+inputPrecioContado = document.getElementById("inputPrecioContado");
+
+function calcular() {
+
+    let precioContado = document.getElementById("inputPrecioContado").value;
+    let precioCuotas = document.getElementById("inputPrecioCuotas").value;
+    let inflacion = document.getElementById("inputValorInflacion").value;
+    let cantidadCuotas = document.getElementById("inputCantidadCuotas").value;
+
+    guardarEnStorage("precioContado",precioContado);
+    guardarEnStorage("precioCuotas",precioCuotas);
+    guardarEnStorage("inflacion",inflacion);
+    guardarEnStorage("cantidadCuotas",cantidadCuotas);
+
+    alert(recuperareStorage("cantidadCuotas"));
+
+    const arrayCuotas = [];
+
+    let precioCuotasAjustado = 0;
+
+    for (let index = 0; index < cantidadCuotas; index++) {
+        let cuota = index + 1;
+        arrayCuotas[index] = ((precioCuotas / cantidadCuotas) / ((1+(inflacion/100)) ** cuota));
+        precioCuotasAjustado+=arrayCuotas[index];
+    }
+
+    let mejorOpcion;
+
+    if (precioContado>precioCuotasAjustado) {
+        mejorOpcion = "Te conviene comprar en cuotas";
+    }else{
+        mejorOpcion = "Te conviene comprar al contado";
+    }
+
+    //Creo mi objeto resultadosFinales:
+
+    const resultadosFinales = {
+        mejorOpcion: mejorOpcion,
+        cuotaAjustadaHoy: precioCuotasAjustado,
+        precioContado: precioContado,
+        precioCuotas: precioCuotas,
+        valorCadaCuota: precioContado / cantidadCuotas,
+        inflacion: inflacion,
+        cuotas: arrayCuotas,
+    };
+
+    //TO DO: Agregar el texto que se muestra antes del valor:
+
+    resultadosFinales1Element.innerHTML = resultadosFinales.mejorOpcion;
+    resultadosFinales2Element.innerHTML = resultadosFinales.cuotaAjustadaHoy;
+    resultadosFinales3Element.innerHTML = resultadosFinales.precioContado;
+    resultadosFinales4Element.innerHTML = resultadosFinales.precioCuotas;
+    resultadosFinales5Element.innerHTML = resultadosFinales.cuotas;
+
+    //TO DO: recorrer cada item de cuotas, para mostrarlo como columna:
+
+    for (let i = 0; i < resultadosFinales.cuotas.length; i++) {
+        const cuota = resultadosFinales.cuotas[i];
+        alert(cuota);
+    }
+}
+
+function RecuperarUltimo(){
+    inputPrecioContado.value = recuperareStorage("precioContado");
+}
+
+//Guardo en Storage:
+
+function guardarEnStorage(key, value) {
+    localStorage.setItem(key, value);
+    sessionStorage.setItem(key,value);
+}
+
+//Recupero del Storage:
+
+function recuperareStorage(key) {
+    return localStorage.getItem(key);
+    //return sessionStorage.getItem(key);
+}
+
+btnCalcular.addEventListener('click', () => {
+    calcular();
+});
+
+btnRecuperarUltimo.addEventListener('click', () => {
+    RecuperarUltimo();
+});
